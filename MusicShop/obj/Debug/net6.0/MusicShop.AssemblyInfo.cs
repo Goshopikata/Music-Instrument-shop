b0cc59106843db67ede319b8e0df1ff009b5a772
Goshopikata <guyfiery53@gmail.com>
2025-05-23 08:24:00 +0200

    Added support email to info page

--- MusicShop/obj/Debug/net6.0/MusicShop.AssemblyInfo.cs
+++ MusicShop/obj/Debug/net6.0/MusicShop.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ff74570de03e91fdbf06ad8cb988fa267bcc5d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03993b3d575a27c4f1706ea64224cb052baf8da2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
